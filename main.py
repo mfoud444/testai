1ff5b848e7d7a241f7d0ab02424339d3a92c017f
Mohammed Foud <mfoud444@gmail.com>
2024-04-10 04:39:27 +0300

    first commit

--- main.py
+++ main.py
@@ -4,21 +4,19 @@ from keras.models import load_model
 import numpy as np
 from PIL import Image
 import io
-
+import os
 app = FastAPI()
 
 # Load the Keras model
-model = load_model('keras_model.h5')  # Replace 'your_model.h5' with the path to your .h5 file
+model_path = os.path.join(os.getcwd(), 'keras_model.h5')
 
-# Function to preprocess the input image
 def preprocess_image(img):
-    img = img.resize((224, 224))  # Assuming input size of 224x224
+    img = img.resize((224, 224))  
     img_array = np.array(img)
-    img_array = img_array.astype('float32') / 255  # Normalization
+    img_array = img_array.astype('float32') / 255 
     img_array = np.expand_dims(img_array, axis=0)
     return img_array
 
-# Define a function to predict the class of an image
 def predict_class(img):
     processed_image = preprocess_image(img)
     prediction = model.predict(processed_image)
@@ -29,13 +27,7 @@ async def predict(file: UploadFile = File(...)):
     contents = await file.read()
     img = Image.open(io.BytesIO(contents))
     prediction = predict_class(img)
-    
-    # Assuming your model output is a list of probabilities for each class
-    # You may need to modify this based on your model's output
     prediction = prediction.tolist()[0]
-    
-    # Assuming you have two classes: Blight disease and Powdery mildew
-    # Modify this based on your actual class names
     class_names = ["Blight disease on grape leaves", "Powdery mildew on grapes"]
     result = {"prediction": class_names[np.argmax(prediction)], "probabilities": prediction}
     return result
@@ -49,7 +41,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Handle OPTIONS requests
 @app.options("/predict/")
 async def options_predict():
     return {"methods": ["POST"]}
